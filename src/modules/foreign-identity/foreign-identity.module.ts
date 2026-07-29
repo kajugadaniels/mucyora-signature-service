caf3df49a15e2645199ae4a618c57a440983b57e
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-07-29 17:26:33 +0200

    refactor(signature): remove foreign identity module

--- src/modules/foreign-identity/foreign-identity.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { ForeignIdentityClient } from './foreign-identity.client';
-
-@Global()
-@Module({
-  providers: [ForeignIdentityClient],
-  exports: [ForeignIdentityClient],
-})
-export class ForeignIdentityModule {}
